16549015f4daa871414e01aa159d0ce9017349bc
Ericrjz <ericvrssm@gmail.com>
2025-01-29 14:18:18 -0300

    added the feature list, to list all the tasks inside the .json

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Task manager for CLI')
 
-parser.add_argument("title", type=str)
-parser.add_argument("state", type=str)
 
-args = parser.parse_args()
+#general commands
+parser.add_argument("-t", "--title")
+parser.add_argument("-s","--state")
+parser.add_argument('-l', '--list')
 
 
+def liste():
+    with open('data.json', 'r') as f:
+         data = json.load(f)
+    if isinstance(data, list):
+        for sublist in data:
+            if isinstance(sublist, list):
+                for item in sublist:
+                    if isinstance(item, dict) and "id" in item:
+                        print(f"{item["id"]}. {item['title']}")
 
 def add_task(title, status):
+
     try:
         with open('data.json', 'r') as f:
             data = json.load(f)
@@ -34,6 +45,7 @@ def add_task(title, status):
         json.dump(data, f, indent=4)
 
 
-
+args = parser.parse_args()
 if __name__ == '__main__':
     add_task(args.title, args.state)
+    liste(args.list)
