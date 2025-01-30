263a8e1f28400a2f605cc3b574fdc32664d45c50
Ericrjz <ericvrssm@gmail.com>
2025-01-30 13:47:58 -0300

    added the feature of excluding tasks 100% functional

--- main.py
+++ main.py
@@ -20,16 +20,14 @@ parser.add_argument("-a", "--addtask", action="store_true", help="add a task to 
 parser.add_argument("-t", "--title")
 parser.add_argument("-s","--state")
 parser.add_argument('-l', '--list', action="store_true", help="list all the tasks by id and their status")
-
+parser.add_argument('-rm', '--remove', action="store_true", help="remove a task based on an ID")
+parser.add_argument('-i', '--id', type=int)
 
 def liste():
     data = setup()
     if isinstance(data, list):
-        for sublist in data:
-            if isinstance(sublist, list):
-                for item in sublist:
-                    if isinstance(item, dict) and "id" in item:
-                        print(f"{item["id"]}. {item['title']} : {item['status']}")
+        for item in data:
+            print(f"{item["id"]}. {item['title']} : {item['status']}")
 
 def add_task(title, status):
 
@@ -37,20 +35,40 @@ def add_task(title, status):
     id = len(data)+1
     task = {
             'title': f'{title}',
-            'id': f'{id}',
+            'id': id,
             'status': f'{status}'
         }
     
     process = []
     process.append(task)
-    data.append(process)
+    data.append(task)
 
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
 
 def remove_task(id):
-    setup()
+    index = id-1
+    new_data = []
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    
+    for i in range(len(data)):
+        if index == i:
+            i+=1
+        else:
+            new_data.append(data[i])
+            for j in range(len(new_data)):
+                temp = j
+                reord = new_data[temp]
+                reord['id'] = j+1
+                j+=1
+            i+=1
+    
 
+
+
+    with open('data.json', 'w') as f:
+            json.dump(new_data, f, indent=4)
 
 
 
@@ -58,5 +76,7 @@ args = parser.parse_args()
 if __name__ == '__main__':
     if args.addtask == True:
         add_task(args.title, args.state)
-    if args.list == True:
+    elif args.list == True:
         liste()
+    elif args.remove == True:
+        remove_task(args.id)
